80ca64caf4199c7773531b79504a94cbec9038ac
puppeymin <289007743@qq.com>
2015-06-24 22:41:12 +0800

    导入greenDAO,删除第一版本

---
+++ badmintonManager/GreenDAOGenerator/src/de/greenrobot/dao/Main.java
@@ -0,0 +1,50 @@
+package de.greenrobot.dao;
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Schema schema = new Schema(1, "de.greenrobot.dao");
+		schema.setDefaultJavaPackageTest("de.greenrobot.daoexample.test");
+		schema.setDefaultJavaPackageDao("de.greenrobot.daoexample.dao");
+		
+		schema.enableKeepSectionsByDefault();
+		
+		addNote(schema);
+		
+		try {
+			new DaoGenerator().generateAll(schema, "../badmintonManager_v0.1/src");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void addNote(Schema schema) {
+        Entity match = schema.addEntity("Match");
+        
+        match.setTableName("MatchTable");
+        
+        match.addIdProperty().primaryKey().autoincrement();
+        match.addDateProperty("time");
+        match.addStringProperty("area");
+        match.addStringProperty("manager");
+        match.addStringProperty("menbers");
+        match.addStringProperty("paidMenbers");
+        
+        Entity member = schema.addEntity("Member");
+        
+        member.setTableName("MemberTable");
+        
+        member.addIdProperty().primaryKey().autoincrement();
+        member.addStringProperty("name");
+        member.addStringProperty("balance");
+        member.addStringProperty("grandTotal");
+	}
+}
